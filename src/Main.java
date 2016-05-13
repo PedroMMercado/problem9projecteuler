216aad71553b6f4123ee19619b91bcd48acf2dda
Pedro <pedrommercado@gmail.com>
2016-05-13 02:28:34 -0400

    Fixed issues with non-naturla numbers

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,6 @@ public class Main {
 		// Find the product abc.
 		// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which
 		int a = 0, b = 0, c = 0;
-		
-//		while(true){
-//			++a;
-//			++b;
-//			c = 1000 - a - b;
-//			if(a * a + b * b == c * c && a < b && b < c)
-//				break;
-//		}
 		for(a = 1; a < 1000;a++){
 			for(b = a + 1; b < 1000;b++){
 				c = 1000 - a - b;
