c4066888942284fc3e55ef819b5ab4678698f31a
Pedro <pedrommercado@gmail.com>
2016-05-13 02:27:12 -0400

    Fixed issues with non-natural numbers

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,25 @@ public class Main {
 		// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 		// Find the product abc.
 		// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which
-		int a = 1, b = a + 1, c = 0;
+		int a = 0, b = 0, c = 0;
 		
-		while(true){
-			++a;
-			++b;
-			c = 1000 - a - b;
-			if(a * a + b * b == c * c && a < b && b < c)
-				break;
+//		while(true){
+//			++a;
+//			++b;
+//			c = 1000 - a - b;
+//			if(a * a + b * b == c * c && a < b && b < c)
+//				break;
+//		}
+		for(a = 1; a < 1000;a++){
+			for(b = a + 1; b < 1000;b++){
+				c = 1000 - a - b;
+				if((a * a) + (b * b) == (c * c) && (a + b + c) == 1000){
+					System.out.println("The value of a is " + a);
+					System.out.println("The value of b is " + b);
+					System.out.println("The value of c is " + c);
+				}
+			}
 		}
-		System.out.println("Value of a is " + a);
-		System.out.println("Value of b is " + b);
-		System.out.println("Value of c is " + c);
-		System.out.println((a + b + c));
-		System.out.println("a^2 + b^2 is " + (a * a) + (b * b));
-		System.out.println("c^2 is " + (c * c));
 
 	}
 
